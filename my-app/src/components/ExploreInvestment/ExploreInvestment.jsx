4bba16b44c9f5eb5cdbb9a2e3735ca782549a041
Mohd Anas <mohammadanasquraishi78600@gmail.com>
2022-11-10 09:01:04 +0530

    Explore INvestment Page

---
+++ my-app/src/components/ExploreInvestment/ExploreInvestment.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Whatis from './Whatis'
+import WhyInvest from './WhyInvest'
+
+function ExploreInvestment() {
+  return (
+     <Box>
+        <WhyInvest/>
+        <Whatis/>
+     </Box>
+  )
+}
+
+export default ExploreInvestment
