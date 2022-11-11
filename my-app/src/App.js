e18cf3e6157dd3269a566df80159450bf1a70c94
NITESH SINDHU <niteshsindhu1435@gmail.com>
2022-11-11 16:11:04 +0530

    day-4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-// import Faq from './Components/faq';
-// import Terms from './Components/terms';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Faq /> */}
-      {/* <Terms/> */}
+    
     </div>
   );
 }
